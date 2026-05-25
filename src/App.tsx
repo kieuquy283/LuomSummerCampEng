b8374f8c9e5ee967f9544c400fa537495c6f3f57
kieuquyvp@gmail.com <kieuquyvp@gmail.com>
2026-05-25 21:24:16 +0700

    fix: remove unused import causing TS6133 build failure

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Activities from './components/Activities';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Hero from './components/Hero';
-import PastSeasonsGallery from './components/PastSeasonsGallery';
+// import PastSeasonsGallery from './components/PastSeasonsGallery';
 import RegistrationOptions from './components/RegistrationOptions';
 import RecruitmentDepartments from './components/RecruitmentDepartments';
 import VolunteerRegistrationForm from './components/VolunteerRegistrationForm';
